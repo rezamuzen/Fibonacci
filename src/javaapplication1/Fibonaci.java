e9e77ab5ec217069d2d80e6ff4e2f860698a5d9b
rezza muntaha zein <37772724+rezza-6706164054@users.noreply.github.com>
2018-03-26 09:26:45 +0700

    Add files via upload

---
+++ src/javaapplication1/Fibonaci.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+
+
+public class Fibonaci {
+    public static void main(String[]args) {
+        Scanner show = new Scanner(System.in);
+        System.out.print("Input: \n");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+        System.out.println("Ouput: ");
+         
+        for(int i = 2; i < n; i++) {
+           
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+ 
+}
